1a379d682cc972720a30bdb9e93041403d3f3240
jingjing7065 <52123426+jingjing7065@users.noreply.github.com>
2025-11-07 12:31:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
